70d14ab1cef5d49931762e8dcab747d5904d9132
cscxxx <167551334+cscxxx@users.noreply.github.com>
2026-01-11 23:05:53 +0800

    添加基础组件，添加示例项目

---
+++ examples/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+// import "./App.css";
+import { Button } from "@csc-react-components/components";
+import React from "react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ packages/components/button/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = () => {
+  return <button>Click me</button>;
+};
+
+Button.displayName = "Button";
+
+export default Button;
